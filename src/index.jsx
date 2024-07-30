c2f156ad3e9478608c844b21070d21ac2996ace5
Oleg Vintoniuk <oleg.vintonuk@gmail.com>
2024-07-30 10:55:57 +0300

    using React.createElement()

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM
   .createRoot(document.querySelector('#root'))
-  .render(<App />);
+  .render(React.createElement(App));
+// .render(<App />);
